ef7507c7e1f9da19ef17e08c3bb262aad9554446
NithinSravan <56395258+NithinSravan@users.noreply.github.com>
2020-08-20 03:28:33 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,12 +10,40 @@ let tanbtn=document.getElementById("tan");
 let x, y;
 let scale=50;
 let clicked=[];
-let i=0;
+let j=0;
 let color=[];
+
+
+class InputWrap{
+    constructor(j){
+        this.index=j;
+    }
+    set(){
+        clicked[this.index]=false;
+        funcInput[this.index].addEventListener("click",()=>{
+            if(!clicked[this.index]){
+                funcInput[this.index].style.border="none";
+                funcInput[this.index].style.borderBottom="2px solid green";
+                clicked[this.index]=true;
+            }
+            else {
+                funcInput[this.index].style.border="none";
+                funcInput[this.index].style.borderBottom="2px solid blue";
+                clicked[this.index]=false;
+            }
+        })
+        colorPicker[this.index].addEventListener("change",()=>{
+            console.log(this.index)
+            color[this.index]=colorPicker[this.index].value;
+            draw();
+        })
+    }
+}
 window.onload=()=>{
-    canvas.width=window.innerWidth;
-    canvas.height=window.innerHeight;
+    canvas.width=0.95*window.innerWidth;
+    canvas.height=0.95*window.innerHeight;
     clicked[0]=false;
+    drawAxes()
     funcInput[0].addEventListener("click",()=>{
         if(!clicked[0]){
             funcInput[0].style.border="none";
@@ -30,6 +58,7 @@ window.onload=()=>{
     })
     colorPicker[0].addEventListener("change",()=>{
         color[0]=colorPicker[0].value;
+        console.log(j)
         draw()
     })
 }
@@ -39,7 +68,6 @@ function graph(){
 }
 function draw(){
     let canvy;
-    console.log(scale)
     ctx.clearRect(0,0,canvas.width,canvas.height)
     for(let i=0;i<funcInput.length;i++){
         for(let posx=-canvas.width/2;posx<canvas.width/2;){
@@ -64,7 +92,90 @@ function draw(){
             ctx.stroke();
         }
     }
+    drawAxes()
 
+}
+function drawAxes(){
+    // ctx.clearRect(0,0,canvas.width,canvas.height)
+    let canvy;
+    let canvx;
+
+    for(let posy=0;posy<canvas.height/2;){
+
+        ctx.beginPath();
+        ctx.strokeStyle="#fff";
+        ctx.lineCap="butt";
+        ctx.lineWidth=2;
+
+        canvy=posy+canvas.height/2;
+        ctx.moveTo(canvas.width/2,canvy);
+       
+        posy+=scale;
+        canvy=posy+canvas.height/2;
+
+        ctx.lineTo(canvas.width/2,canvy);
+        ctx.moveTo(canvas.width/2,canvy);
+        ctx.lineTo(canvas.width/2-10,canvy);
+        ctx.stroke();
+      
+    }
+    for(let posx=0;posx<canvas.width/2;){
+
+        ctx.beginPath();
+        ctx.strokeStyle="#fff";
+        ctx.lineCap="butt";
+        ctx.lineWidth=2;
+
+        canvx=posx+canvas.width/2;
+        ctx.moveTo(canvx,canvas.height/2);
+       
+        posx+=scale;
+        canvx=posx+canvas.width/2;
+
+        ctx.lineTo(canvx,canvas.height/2);
+        ctx.moveTo(canvx,canvas.height/2);
+        ctx.lineTo(canvx,canvas.height/2-10)
+        ctx.stroke();
+    }
+
+
+
+    for(let posy=0;posy>-canvas.height/2;){
+
+        ctx.beginPath();
+        ctx.strokeStyle="#fff";
+        ctx.lineCap="butt";
+        ctx.lineWidth=2;
+
+        canvy=posy+canvas.height/2;
+        ctx.moveTo(canvas.width/2,canvy);
+       
+        posy-=scale;
+        canvy=posy+canvas.height/2;
+
+        ctx.lineTo(canvas.width/2,canvy);
+        ctx.moveTo(canvas.width/2,canvy);
+        ctx.lineTo(canvas.width/2-10,canvy)
+        ctx.stroke();
+    }
+    for(let posx=0;posx>-canvas.width/2;){
+
+        ctx.beginPath();
+        ctx.strokeStyle="#fff";
+        ctx.lineCap="butt";
+        ctx.lineWidth=2;
+
+        canvx=posx+canvas.width/2;
+        ctx.moveTo(canvx,canvas.height/2);
+       
+        posx-=scale;
+        canvx=posx+canvas.width/2;
+
+        ctx.lineTo(canvx,canvas.height/2);
+        ctx.moveTo(canvx,canvas.height/2);
+        ctx.lineTo(canvx,canvas.height/2-10)
+        ctx.stroke();
+    }
 }
 function addFunc(){
     const div=document.createElement("div");
@@ -84,29 +195,13 @@ function addFunc(){
     div.appendChild(pickerInput);
     pickerInput.classList.add("color-picker");
 
-    i++;
-    clicked[i]=false;
-    inputEle.addEventListener("click",()=>{
-        if(!clicked[i]){
-            funcInput[i].style.border="none";
-            funcInput[i].style.borderBottom="2px solid green";
-            clicked[i]=true;
-        }
-        else {
-            funcInput[i].style.border="none";
-            funcInput[i].style.borderBottom="2px solid blue";
-            clicked[i]=false;
-        }
-    })
-    pickerInput.addEventListener("change",()=>{
-        color[i]=pickerInput.value;
-        draw()
-    })
-
+    j++;
+    let inputWrapper=new InputWrap(j);
+    inputWrapper.set();
 }
 function trig(fn){
     for(let i=0;i<funcInput.length;i++){
-        if(clicked[i]){
+        if(funcInput[i].style.borderBottom==="2px solid green"){
             switch(fn){
                 case 'sin':{
                     funcInput[i].value+="Math.sin(x)";
@@ -134,10 +229,12 @@ function trig(fn){
 window.addEventListener('keydown', function (e) {
     if (e.key === "w") {
         scale+=5;
+        drawAxes()
         draw()
     }
     else if(e.key==="s"){
         scale-=5;
+        drawAxes()
         draw()
     }
 
